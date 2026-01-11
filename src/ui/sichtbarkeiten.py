12dffd102c0b7f6bc93c61419740b8544e59c4b1
Huber.Johannes <johannes.huber@student.htw-berlin.de>
2026-01-11 23:16:45 +0100

    Git Sichtbarkeiten für C-C-Online fixed

--- src/ui/sichtbarkeiten.py
+++ src/ui/sichtbarkeiten.py
@@ -5,13 +5,22 @@ class Sichtbarkeiten:
     @staticmethod
     def get_sichtbarkeit(modus: Modus) -> dict:
         """Gibt ein Dictionary mit Sichtbarkeits-Einstellungen für den gegebenen Modus zurück"""
-        if modus == Modus.C_M_ONLINE or modus == Modus.C_C_ONLINE:
+        if modus == Modus.C_M_ONLINE:
             return {
                 "show_farbe": False,
                 "show_algorithmus": False,
                 "show_zeit": False,
                 "show_code_auswahl": False,
                 "show_code_auswahl_spiel_ui": True,
+                "show_knuth_vs_step": False  # Neue Eigenschaft
+            }
+        elif modus == Modus.C_C_ONLINE:
+            return {
+                "show_farbe": False,
+                "show_algorithmus": False,
+                "show_zeit": False,
+                "show_code_auswahl": False,
+                "show_code_auswahl_spiel_ui": False,
                 "show_knuth_vs_step": False  # Neue Eigenschaft
             }
         elif modus == Modus.C_M:  # Expliziter Vergleich statt `in`
